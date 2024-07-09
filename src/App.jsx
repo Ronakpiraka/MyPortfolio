dc57137c2207aef83e5306d3078876d93c29cbc4
Ronakpiraka <ronakrajesh1904@gmail.com>
2024-07-09 21:23:29 +0530

    try this now

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import { BrowserRouter ,Routes, Route} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import Experience from './components/Experience';
+import About from './components/About';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar/>
+    <Router>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/experience' element={<Experience/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/experience' element={<Experience />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
-      <Footer/>
-    </BrowserRouter>
+      <Footer />
+    </Router>
   );
 }
 
