74f7d054484ee5d34f6355813b72e44bca5a0c84
Ronakpiraka <ronakrajesh1904@gmail.com>
2024-09-16 01:01:27 +0530

    Updated resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -37,17 +37,16 @@ const About = () => {
         <div className='flex flex-col justify-around'>
           <div className='sm:px-16 px-2'>
             <h2 className='text-4xl sm:text-5xl font-extrabold mt-2'>Introduction</h2>
-            <p className='mt-3 mb-6 text-[17px] max-w-3xl leading-[30px]'>👨‍💻 Hi, I'm Ronak Piraka, a results-oriented software development professional with expertise in React.js, Python, Java, and MySQL. I have a proven track record in front-end development, cloud deployment (AWS, GCP), and cross-functional team collaboration. 
+            {/* <p className='mt-3 mb-6 text-[17px] max-w-3xl leading-[30px]'>👨‍💻 Hi, I'm Ronak Piraka, a results-oriented software development professional with expertise in React.js, Python, Java, and MySQL. I have a proven track record in front-end development, cloud deployment (AWS, GCP), and cross-functional team collaboration. 
               <br/>
               🚀 As a seasoned <a className='text-green-300 hover:text-green-500 duration-300' href='https://linkedin.com/in/ronak-piraka' target='_blank'>software developer</a>, I specialize in both MEAN and MERN stacks. 
               <br/>
-              ✍️ Beyond coding, I love interacting with people and sharing my experiences.</p>
-              <p>Results-driven Software Engineer with more than 1.5 years of experience in full-stack development, specializing in React.js, TypeScript, Java, and cloud technologies (AWS, GCP). Proven expertise in developing user-centric applications, optimizing backend processes, and implementing no-code/low-code solutions. Adept at cross-functional collaboration and driving efficiency improvements.</p>
+              ✍️ Beyond coding, I love interacting with people and sharing my experiences.</p> */}
+              <p className='mt-3 mb-6 text-[17px] max-w-3xl leading-[30px]'> 👨‍💻 Hi, I'm Ronak Piraka, Results-driven Software Engineer with more than 1.5 years of experience in full-stack development, specializing in React.js, TypeScript, Java, and cloud technologies (AWS, GCP).✍️ Proven expertise in developing user-centric applications, optimizing backend processes, and implementing no-code/low-code solutions.  🚀 Adept at cross-functional collaboration and driving efficiency improvements.</p>
 
             <div className='flex gap-4'>
               <ButtonLink
-                // url='https://drive.google.com/file/d/1eonTrnoOUA1Vd5ruCcbhEtKbgjmnOscU/view?usp=drive_link'
-                url='https://drive.google.com/file/d/17iB1hd1nELKTNHHeocpN9s1a_oGMbme5/view?usp=sharing'
+                url='https://drive.google.com/drive/folders/15bL9y3C-eg6rNrQCgQqIoS_IN28l4fuP?usp=drive_link'
                 text='View Resume →'
                 padding={`p-3`}
               />
